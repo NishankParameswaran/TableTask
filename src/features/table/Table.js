c01efcb25a6c102cf5c754715df43aff99e16b7c
NishankParameswaran <nishankrko115@gmail.com>
2021-03-04 21:25:31 +0700

    debug

--- src/features/table/Table.js
+++ src/features/table/Table.js
@@ -3,7 +3,7 @@ import BootstrapTable from 'react-bootstrap/Table'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useSelector, useDispatch } from 'react-redux';
 import {
-setTableData,fetchTableData, setStatus
+fetchTableData, setStatus
 } from './tableSlice';
 import Dropdown from 'react-bootstrap/Dropdown'
 
@@ -29,46 +29,48 @@ function Table() {
     }) : tableData 
   return (
     <div className="App">
-    <Dropdown>
-    <Dropdown.Toggle variant="success" id="dropdown-basic">
-        Status
-    </Dropdown.Toggle>
-
-    <Dropdown.Menu>
-    <Dropdown.Item onClick={()=> {
-                 dispatch(setStatus("All"))
-             }}>{"All"}</Dropdown.Item>
-        {statuses.map((item) => {
-            return(
-             <Dropdown.Item onClick={()=> {
-                 dispatch(setStatus(item))
-             }}>{item}</Dropdown.Item>
-            )
-        })}
-    </Dropdown.Menu>
-    </Dropdown>
+        <div className="drop-down" style={{textAlign: "right"}}>
+            <Dropdown>
+             <Dropdown.Toggle variant="success" id="dropdown-basic">
+               Status
+             </Dropdown.Toggle>
+            <Dropdown.Menu>
+            <Dropdown.Item onClick={()=> {
+                    dispatch(setStatus("All"))
+                }}>{"All"}
+            </Dropdown.Item>
+            {statuses.map((item) => {
+                return(
+                <Dropdown.Item onClick={()=> {
+                    dispatch(setStatus(item))
+                }}>{item}</Dropdown.Item>
+                )
+                })}
+            </Dropdown.Menu>
+            </Dropdown>
+        </div>
       <BootstrapTable striped bordered hover>
-  <thead>
-    <tr>
-      <th>Project Id</th>
-      <th>Project Code</th>
-      <th>Status</th>
-    </tr>
-  </thead>
-  <tbody>
-    {renderTableData &&
-      renderTableData.map((item,index) => {
-        return (
-          <tr>
-            <td>{item.project_id}</td>
-            <td>{item.project_code}</td>
-            <td>{item.status}</td>
-          </tr>
-        )
-      })
-    }
-  </tbody>
-</BootstrapTable>
+        <thead>
+            <tr>
+            <th>Project Id</th>
+            <th>Project Code</th>
+            <th>Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            {renderTableData &&
+            renderTableData.map((item,index) => {
+                return (
+                <tr>
+                    <td>{item.project_id}</td>
+                    <td>{item.project_code}</td>
+                    <td>{item.status}</td>
+                </tr>
+                )
+            })
+            }
+        </tbody>
+      </BootstrapTable>
     </div>
   );
 }
